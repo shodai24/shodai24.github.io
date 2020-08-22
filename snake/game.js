eb828dd892a5be34405a4364a389c030e3d0f009
William Gatsinzi <williamgatsinzi@yahoo.com>
2020-08-22 13:52:36 +0200

    Added snake game

---
+++ snake/game.js
@@ -0,0 +1,43 @@
+import {update as updateSnake, draw as drawSnake, getSnakeHead, snakeIntersection, SNAKE_SPEED} from './snake.js'
+import {update as updateFood, draw as drawFood} from './food.js'
+import {outsideGrid} from './grid.js'
+
+let lastRenderTime = 0
+let gameOver = false
+const gameBoard = document.getElementById('game-board')
+
+function main(currentTime){
+    if (gameOver) {
+        if (confirm('GAME OVER. PRESS OK TO RESTART')) {
+            window.location.reload()
+        }
+        else window.close()
+    }
+    window.requestAnimationFrame(main)
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+    if(secondsSinceLastRender < 1 / SNAKE_SPEED) return
+
+    console.log("Render")
+    lastRenderTime = currentTime
+
+    update()
+    draw()
+}
+
+window.requestAnimationFrame(main)
+
+function update() {
+    updateSnake()
+    updateFood()
+    checkDeath()
+}
+
+function draw() {
+    gameBoard.innerHTML = ''
+    drawSnake(gameBoard)
+    drawFood(gameBoard)
+}
+
+function checkDeath() {
+    gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
+}
